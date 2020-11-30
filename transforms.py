031f5bdd84f3ac40c975c7810e1e30dcc733af1b
Hugo Lebeau <hug97@hotmail.fr>
2020-11-30 19:22:10 +0100

    Image transforms between RGB and L*a*b* + simple data transforms

---
+++ transforms.py
@@ -0,0 +1,106 @@
+import torch
+from torchvision import transforms
+
+def rgb2lab(img_rgb):
+    '''
+    RGB to L*a*b* conversion.
+
+    Parameters
+    ----------
+    img_rgb : torch.tensor
+        (3, H, W) RGB image.
+
+    Returns
+    -------
+    torch.tensor
+        (3, H, W) image in the L*a*b* color space.
+
+    '''
+    img = img_rgb.detach().clone()
+    # RGB > XYZ
+    mask = img > 0.04045
+    img[mask] = ((img[mask]+0.055)/1.055)**2.4
+    img[~mask] /= 12.92
+    X = img[0]*0.412453+img[1]*0.357580+img[2]*0.180423
+    Y = img[0]*0.212671+img[1]*0.715160+img[2]*0.072169
+    Z = img[0]*0.019334+img[1]*0.119193+img[2]*0.950227
+    # XYZ > L*a*b*
+    X, Y, Z = X/0.95047, Y, Z/1.08883 # CIE Standard Illuminant D65, 2° observer
+    img_xyz = torch.stack([X, Y, Z])
+    mask = img_xyz > 0.008856
+    img_xyz[mask] = img_xyz[mask]**(1./3.)
+    img_xyz[~mask] = 7.787*img_xyz[~mask]+16./116.
+    L = 116.*img_xyz[1]-16.
+    a = 500.*(img_xyz[0]-img_xyz[1])
+    b = 200.*(img_xyz[1]-img_xyz[2])
+    return torch.stack([L, a, b])
+
+def lab2rgb(img_lab):
+    '''
+    L*a*b* to RGB conversion.
+
+    Parameters
+    ----------
+    img_lab : torch.tensor
+        (3, H, W) L*a*b* image.
+
+    Returns
+    -------
+    img_rgb : torch.tensor
+        (3, H, W) image in RGB.
+
+    '''
+    img = img_lab.detach().clone()
+    # L*a*b* > XYZ
+    Y = (img[0]+16.)/116.
+    X, Z = img[1]/500.+Y, Y-img[2]/200.
+    img_xyz = torch.stack([X, Y, Z])
+    mask = img_xyz > 0.2068966
+    img_xyz[mask] = img_xyz[mask]**3
+    img_xyz[~mask] = (img_xyz[~mask]-16./116.)/7.787
+    img_xyz[0], img_xyz[1], img_xyz[2] = img_xyz[0]*0.95047, img_xyz[1], img_xyz[2]*1.08883 # CIE Standard Illuminant D65, 2° observer
+    # XYZ > RGB
+    R = img_xyz[0]*3.240481+img_xyz[1]*-1.537152+img_xyz[2]*-0.498536
+    G = img_xyz[0]*-0.969255+img_xyz[1]*1.87599+img_xyz[2]*0.041556
+    B = img_xyz[0]*0.055647+img_xyz[1]*-0.204041+img_xyz[2]*1.057311
+    img_rgb = torch.stack([R, G, B])
+    mask = img_rgb > 0.0031308
+    img_rgb[mask] = 1.055*(img_rgb[mask]**(1./2.4))-0.055
+    img_rgb[~mask] *= 12.92
+    return img_rgb
+
+def rgb2l(img_rgb):
+    '''
+    RGB to L* convsersion.
+
+    Parameters
+    ----------
+    img_rgb : torch.tensor
+        (3, H, W) RGB image.
+
+    Returns
+    -------
+    torch.tensor
+        (H, W) lightness (L*) image.
+
+    '''
+    img = img_rgb.detach().clone()
+    # RGB > Y
+    mask = img > 0.04045
+    img[mask] = ((img[mask]+0.055)/1.055)**2.4
+    img[~mask] /= 12.92
+    Y = img[0]*0.212671+img[1]*0.715160+img[2]*0.072169
+    # Y > L*
+    Y = Y # CIE Standard Illuminant D65, 2° observer
+    mask = Y > 0.008856
+    Y[mask] = Y[mask]**(1./3.)
+    Y[~mask] = 7.787*Y[~mask]+16./116.
+    L = 116.*Y-16.
+    return L
+
+data_transform = {
+    'gray': transforms.Compose([transforms.ToTensor(),
+                                (lambda img_rgb: rgb2l(img_rgb).view(1, -1, img_rgb.shape[-1]))
+                                ]),
+    'color': transforms.Compose([transforms.ToTensor()
+                                 ])}
