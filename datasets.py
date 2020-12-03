f0a1341ec73f549810de2d3cc758d8e490cad8db
Hugo Lebeau <hug97@hotmail.fr>
2020-12-03 12:08:11 +0100

    Places205 class

---
+++ datasets.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+from torch import utils
+
+class Places205(utils.data.Dataset):
+    def __init__(self, path, transform):
+        super(Places205, self).__init__()
+        self.path = path
+        self.transform = transform
+        self.list_files = [root+'/'+file for root, dirs, files in os.walk(path) for file in files]
+        self.length = None
+    
+    def __len__(self):
+        if self.length is None:
+            self.length = len(self.list_files)
+        return self.length
+    
+    def __getitem__(self, index):
+        return self.transform(Image.open(self.list_files[index]))
