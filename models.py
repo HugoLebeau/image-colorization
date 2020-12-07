c9f55e44579a91d6e776c8636fcd7c7321d1167f
Hugo Lebeau <hug97@hotmail.fr>
2020-12-07 11:44:55 +0100

    typo

--- models.py
+++ models.py
@@ -1,23 +1,23 @@
-import torch.nn as nn
+from torch import nn
 
 class Zhang16(nn.Module):
     def __init__(self):
         super(Zhang16, self).__init__()
-        self.conv1 = nn.Sequential([
+        self.conv1 = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(64)
-            ])
-        self.conv2 = nn.Sequential([
+            )
+        self.conv2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(128)
-            ])
-        self.conv3 = nn.Sequential([
+            )
+        self.conv3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
@@ -25,8 +25,8 @@ class Zhang16(nn.Module):
             nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(256)
-            ])
-        self.conv4 = nn.Sequential([
+            )
+        self.conv4 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
@@ -34,8 +34,8 @@ class Zhang16(nn.Module):
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(512)
-            ])
-        self.conv5 = nn.Sequential([
+            )
+        self.conv5 = nn.Sequential(
             nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
@@ -43,8 +43,8 @@ class Zhang16(nn.Module):
             nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(512)
-            ]) # à trous
-        self.conv6 = nn.Sequential([
+            ) # à trous
+        self.conv6 = nn.Sequential(
             nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
@@ -52,8 +52,8 @@ class Zhang16(nn.Module):
             nn.Conv2d(512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(512)
-            ]) # à trous
-        self.conv7 = nn.Sequential([
+            ) # à trous
+        self.conv7 = nn.Sequential(
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
@@ -61,8 +61,8 @@ class Zhang16(nn.Module):
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(inplace=True),
             nn.BatchNorm2d(512)
-            ])
-        self.conv8 = nn.Sequential([
+            )
+        self.conv8 = nn.Sequential(
             nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
@@ -70,7 +70,7 @@ class Zhang16(nn.Module):
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(256, 313, kernel_size=1, stride=1, padding=0, bias=True)
-            ])
+            )
         self.softmax = nn.Softmax(dim=1)
         self.conv9 = nn.Conv2d(313, 2, kernel_size=1, padding=0, dilation=1, stride=1, bias=False)
         self.upsample = nn.Upsample(scale_factor=4, mode='bilinear')
