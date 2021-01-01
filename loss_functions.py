5fbc2e6484eda45f440aba12bcba68ccaa847d2b
Hugo Lebeau <hug97@hotmail.fr>
2021-01-01 10:22:47 +0100

    smooth L1 (or Huber) loss

--- loss_functions.py
+++ loss_functions.py
@@ -19,4 +19,27 @@ def MCE(prop, target, weights=1.):
         The weighted multinomial cross entropy loss.
 
     '''
-    return -torch.sum(weights*torch.sum(target*torch.log(prop), axis=-1), axis=[-1, -2])
+    return -torch.sum(weights*torch.sum(target*torch.log(prop), axis=-1), dim=(-1, -2))
+
+def smoothL1(prop, target, delta=1.):
+    '''
+    Smooth-L1 (Huber) loss.
+
+    Parameters
+    ----------
+    prop : torch.Tensor, shape (..., C, H, W)
+        Proposal image.
+    target : torch.Tensor, shape (..., C, H, W)
+        Target image.
+    delta : float, optional
+        Steepness parameter. The default is 1..
+
+    Returns
+    -------
+    torch.Tensor
+        The smooth-L1 loss.
+
+    '''
+    diff = torch.abs(prop-target)
+    pointwise_loss = torch.where(diff < delta, 0.5*torch.pow(diff, 2.), delta*(diff-0.5*delta))
+    return torch.sum(pointwise_loss, dim=(-1, -2, -3))
