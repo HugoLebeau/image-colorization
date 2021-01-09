a06b5d7a9cb4870122c0a502a71d184f190a975c
Hugo Lebeau <hug97@hotmail.fr>
2021-01-09 18:19:13 +0100

    add possibility to return the name of the image (False by default)

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -3,7 +3,7 @@ from PIL import Image
 from torch import utils
 
 class Dataset(utils.data.Dataset):
-    def __init__(self, path, transform, directory, rgb_only, maxsize):
+    def __init__(self, path, transform, directory, rgb_only, maxsize, return_name):
         super(Dataset, self).__init__()
         self.path = path
         self.transform = transform
@@ -13,6 +13,7 @@ class Dataset(utils.data.Dataset):
         if maxsize:
             dirmode = dirmode[:maxsize]
         self.directory = dirmode[0].values
+        self.return_name = return_name
         self.length = None
     
     def __len__(self):
@@ -21,16 +22,19 @@ class Dataset(utils.data.Dataset):
         return self.length
     
     def __getitem__(self, index):
-        return self.transform(Image.open(self.path+self.directory[index]))
+        if self.return_name:
+            return self.directory[index], self.transform(Image.open(self.path+self.directory[index]))
+        else:
+            return self.transform(Image.open(self.path+self.directory[index]))
 
 class COCOStuff(Dataset):
-    def __init__(self, path, transform, directory='directory_COCO-Stuff.csv', rgb_only=True, maxsize=None):
-        super(COCOStuff, self).__init__(path, transform, directory, rgb_only, maxsize)
+    def __init__(self, path, transform, directory='directory_COCO-Stuff.csv', rgb_only=True, maxsize=None, return_name=False):
+        super(COCOStuff, self).__init__(path, transform, directory, rgb_only, maxsize, return_name)
 
 class Places205(Dataset):
-    def __init__(self, path, transform, directory='directory_Places205.csv', rgb_only=True, maxsize=None):
-        super(Places205, self).__init__(path, transform, directory, rgb_only, maxsize)
+    def __init__(self, path, transform, directory='directory_Places205.csv', rgb_only=True, maxsize=None, return_name=False):
+        super(Places205, self).__init__(path, transform, directory, rgb_only, maxsize, return_name)
 
 class SUN2012(Dataset):
-    def __init__(self, path, transform, directory='directory_SUN2012.csv', rgb_only=True, maxsize=None):
-        super(SUN2012, self).__init__(path, transform, directory, rgb_only, maxsize)
+    def __init__(self, path, transform, directory='directory_SUN2012.csv', rgb_only=True, maxsize=None, return_name=False):
+        super(SUN2012, self).__init__(path, transform, directory, rgb_only, maxsize, return_name)
