50855112f716864740230b1817acb7c170e8e0b0
Hugo Lebeau <hug97@hotmail.fr>
2021-01-10 15:09:11 +0100

    add not none condition

--- main.py
+++ main.py
@@ -175,8 +175,7 @@ def training(model_name, weights, lr, train_loader, val_loader, val_size, val_st
         loss.backward()
         df['training loss'][ite] = loss.data.item()/data.shape[0]
         count_nan += 1
-        print(np.unique([param.grad is None for param in model.parameters()], return_counts=True))
-        if not np.any([torch.any(torch.isnan(param.grad)).item() for param in model.parameters()]): # if the gradients are not nan
+        if not np.any([torch.any(torch.isnan(param.grad)).item() for param in model.parameters() if param is not None]): # if the gradients are not nan
             count_nan = 0
             df['optimizer step'][ite] = True
             optimizer.step()
