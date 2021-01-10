2aacf0d033a51e9c433299570747f49bf9d1821b
Hugo Lebeau <hug97@hotmail.fr>
2021-01-10 15:48:37 +0100

    debugging

--- main.py
+++ main.py
@@ -174,6 +174,7 @@ def training(model_name, weights, lr, train_loader, val_loader, val_size, val_st
         loss = criterion(output, target)
         loss.backward()
         df['training loss'][ite] = loss.data.item()/data.shape[0]
+        print(loss.data.item())
         count_nan += 1
         if not np.any([torch.any(torch.isnan(param.grad)).item() for param in model.parameters() if param.grad is not None]): # if the gradients are not nan
             count_nan = 0
